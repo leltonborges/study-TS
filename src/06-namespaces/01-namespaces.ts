f2202acd5188d54a068052b538164a6ddca73de9
Lelton Borges <leltondev@gmail.com>
2022-09-01 22:27:54 -0300

    chore: namespaces

---
+++ src/06-namespaces/01-namespaces.ts
@@ -0,0 +1,5 @@
+///<reference path="02-geometriaCirc.ts"/>
+///<reference path="03-geometriaRec.ts"/>
+
+console.log(Geometria.Areas.circunferencia(10));
+console.log(Geometria.Areas.retangulo(10, 20));
---
+++ src/06-namespaces/02-geometriaCirc.ts
@@ -0,0 +1,7 @@
+namespace Geometria {
+    export namespace Areas {
+        export function circunferencia(raio: number): number {
+            return Math.PI * Math.pow(raio, 2)
+        }
+    }
+}
